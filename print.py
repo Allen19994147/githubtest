b5939e3d972f6231721b8304a1463920a7d26224
Allen Lee <115447102+Allen19994147@users.noreply.github.com>
2024-04-20 15:12:19 -0700

    Count number (#4)
    * Added num counter in fruits
    
    * All num count
    
    ---------
    
    Co-authored-by: Allen Lee <allen199994147@gmail.com>

--- print.py
+++ print.py
@@ -4,13 +4,19 @@ fruits = ["apple", "banana", "cherry","kiwi", "pear", "watermelon", "blueberry",
 for x in fruits:
   print(x)
 
+print("The number of fruits is: "+ str(len(fruits)))
 print("\n")
 
 names = ["Allen","Kenny","Jenny","Lucas","John","Elsa","Rock","Bob","Arjun"]
 for x in names:
   print(x)
 
+print("The number of names is: "+ str(len(names)))
 print("\n")
+
 items = ["Umbrella","Pot","Jacket","Jeans","Stove"]
 for x in items:
   print(x)
+
+print("The number of items is: "+ str(len(items)))
+
